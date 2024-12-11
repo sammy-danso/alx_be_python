1caef95666f57f47c953cb37ceb44b0b27410adc
sammy-danso <mr.dansosamuel@icloud.com>
2024-12-11 09:09:41 -0800

    Added simple_interest.py to calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# My first day to use Python to Define the variables
+principal = 1000 # Principal amount
+rate = 0.05       # Annual interest rate
+time = 3          # Time in years
+
+# Calculate simple interest
+interest = principal * rate * time
+
+# Print the result
+print(f"The simple interest is: {interest}")
